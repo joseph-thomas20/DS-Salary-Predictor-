e979e76d432a58c8bdc5ccd16a7bd83a13edb6e4
Joseph Thomas <93582626+joseph-thomas20@users.noreply.github.com>
2022-03-15 19:03:28 +0000

    Create salary_data_cleaning_code.py

--- salary_data_cleaning_code.py
+++ salary_data_cleaning_code.py
@@ -41,25 +41,8 @@ df['age'] = df.Founded.apply(lambda x: x if x <1 else 2020 - x)
 
 
 #Parsing Job Description
-
-#python
 df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
  
-#r studio 
-df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
-df.R_yn.value_counts()
-
-#spark 
-df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
-df.spark.value_counts()
-
-#aws 
-df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
-df.aws.value_counts()
-
-#excel
-df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
-df.excel.value_counts()
 
 df.columns
 
